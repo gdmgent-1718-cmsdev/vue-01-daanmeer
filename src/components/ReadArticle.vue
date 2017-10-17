9a4ffca6d3c225b7d3ebebabb52be906f8127156
daanmeer <daanmeer@arteveldehs.be>
2017-10-17 13:23:11 +0200

    update

---
+++ src/components/ReadArticle.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <h1>
+        {{ msg }}
+    </h1>
+  </div>
+</template>
+<script>
+  import axios from 'axios';
+  export default {
+    name: 'read-article',
+    data() {
+      return {
+        msg: 'Read'
+      }
+    } ,
+    created() {
+      axios
+      .get('http://cmsdev.localhost')
+      .then(({ data: response }) =>
+      {
+        console.log(response)
+      })
+      .catch(({message:error}) => {
+        console.info(error)
+      })
+    }
+  }
+</script>
